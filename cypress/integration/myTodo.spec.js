e0054eeb99abb676ff9a96882e25a5762c39c0f4
jho2301 <jho2301@gmail.com>
2020-12-23 17:36:26 +0900

    test: todo list의 체크박스를 클릭하여 complete 상태로 변경한다

--- cypress/integration/myTodo.spec.js
+++ cypress/integration/myTodo.spec.js
@@ -8,4 +8,11 @@ describe("TodoList E2E테스트", () => {
     cy.get(".new-todo").type(`${newTodoItem}{enter}`);
     cy.get(".label").eq(-1).should("text", newTodoItem);
   });
+
+  // (li tag 에 completed class 추가, input 태그에 checked 속성 추가)
+  it("todo list의 체크박스를 클릭하여 complete 상태로 변경한다", () => {
+    cy.get("#todo-list li .toggle").eq(-1).click();
+    cy.get("#todo-list li").eq(-1).should("have.class", "completed");
+    // cy.get("#todo-list li .toggle").eq(-1).should("have.attr", "checked");
+  });
 });
